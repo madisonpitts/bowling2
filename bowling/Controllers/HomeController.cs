25261d8a1e7381f23ac3ad2b4a236ff1d8c53933
madisonpitts <madpitts@byu.edu>
2022-03-30 20:52:50 -0600

    Initial check-in of module bowling

---
+++ bowling/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using bowling.Models;
+
+namespace bowling.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BowlingDbContext _context { get; set; }
+
+        public HomeController(BowlingDbContext temp)
+        {
+            _context = temp;
+        }
+
+        public IActionResult Index()
+        {
+            var blah = _context.Bowlers.ToList();
+
+            return View(blah);
+        }
+
+        
+    }
+}
---
+++ bowling/Models/BowlingDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace bowling.Models
+{
+    public class BowlingDbContext : DbContext
+    {
+        public BowlingDbContext(DbContextOptions<BowlingDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Bowler> Bowlers { get; set; }
+    }
+}
