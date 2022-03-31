99b92d04bb4761eae3a900a8a1f535bbf30c2b15
madisonpitts <madpitts@byu.edu>
2022-03-31 09:51:52 -0600

    crud

--- bowling/Controllers/HomeController.cs
+++ bowling/Controllers/HomeController.cs
@@ -25,6 +25,79 @@ namespace bowling.Controllers
             return View(blah);
         }
 
-        
+        [HttpGet]
+        public IActionResult Add()
+        {
+            var x = new Bowler();
+
+
+            return View("Add", x);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Bowler b)
+        {
+            if (ModelState.IsValid)
+            {
+
+                _context.Add(b);
+                _context.SaveChanges();
+
+
+                return RedirectToAction("Index");
+            }
+
+            else
+            {
+                return View();
+            }
+
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int bowlerid)
+        {
+
+            var x = _context.Bowlers.Single(x => x.BowlerID == bowlerid);
+
+
+
+            return View("Edit", x);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Bowler b, int bowlerid)
+        {
+            if (ModelState.IsValid)
+            {
+
+                _context.Update(b);
+
+                _context.SaveChanges();
+
+
+
+                return RedirectToAction("Index");
+
+            }
+
+
+            else
+            {
+                return View();
+            }
+
+
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int bowlerid)
+        {
+            Bowler bowler = _context.Bowlers.Single(x => x.BowlerID == bowlerid);
+            _context.Bowlers.Remove(bowler);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
     }
 }
